605bf1d809f05b358db0d5fa9352bfd4e6df37d6
zhengyuli <zhengyuli0321@gmail.com>
2020-06-10 00:07:02 -0700

    Add and Delete Course Section

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduVideoController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduVideoController.java
@@ -1,8 +1,12 @@
 package com.atguigu.eduservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.EduVideo;
+import com.atguigu.eduservice.service.EduVideoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -12,8 +16,26 @@ import org.springframework.web.bind.annotation.RestController;
  * @author zli78122
  * @since 2020-06-08
  */
+@Api(tags = {"课程小节管理"})
 @RestController
-@RequestMapping("/eduservice/edu-video")
+@RequestMapping("/eduservice/video")
+@CrossOrigin //解决跨域问题
 public class EduVideoController {
 
+    @Autowired
+    private EduVideoService videoService;
+
+    @ApiOperation(value = "添加小节")
+    @PostMapping("/addVideo")
+    public R addVideo(@RequestBody EduVideo eduVideo) {
+        videoService.save(eduVideo);
+        return R.ok();
+    }
+
+    @ApiOperation(value = "删除小节")
+    @DeleteMapping("/{id}")
+    public R deleteVideo(@PathVariable String id) {
+        videoService.removeById(id);
+        return R.ok();
+    }
 }
